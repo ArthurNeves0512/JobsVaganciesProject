2673c6c3c0213f8a7770ca3f9602432f70be2585
Arthur Neves <arthur@pop-os.localdomain>
2022-03-31 16:17:29 -0300

    Primeiro Commit

---
+++ vagas-emprego/src/test/java/br/com/industriaisneves/vagasemprego/VagasEmpregoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.industriaisneves.vagasemprego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasEmpregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
